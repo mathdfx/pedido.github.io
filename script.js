cc63445da9585f4e86685ca3e8636e893ec29fbd
mathdfx <134085449+mathdfx@users.noreply.github.com>
2025-02-05 22:42:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 const botaoSim = document.getElementById('sim');
 const botaoNao = document.getElementById('nao');
 
-// Redireciona ao clicar no botão "Sim"
 botaoSim.addEventListener('click', () => {
     window.location.href = 'https://www.youtube.com/watch?v=PR0y3pKo8-8';
 });
 
-// Faz o botão "Não" fugir
 botaoNao.addEventListener('mouseover', () => {
     const x = Math.random() * (window.innerWidth - botaoNao.offsetWidth);
     const y = Math.random() * (window.innerHeight - botaoNao.offsetHeight);
@@ -14,6 +12,7 @@ botaoNao.addEventListener('mouseover', () => {
     botaoNao.style.top = `${y}px`;
 });
 
-botaoNao.addEventListener('click', () => {
-    alert('Você clicou em "Não"!');
+botaoSim.addEventListener('click', () => {
+    alert('É NADA');
 });
+
